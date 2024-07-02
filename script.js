34e130694e4fab3a49e4f450ef66de70d4caf28e
Kushal Parajuli <155858513+Kushalpc@users.noreply.github.com>
2024-07-02 12:32:51 +0545

    write logic for comparison and declaration of results

--- script.js
+++ script.js
@@ -4,13 +4,50 @@ function getComputerChoice(){
   let randomNumber = Math.round(Math.random() * 1000) / 100;
 
   if(randomNumber <= 3.33){
-    return "Rock";
+    return "rock";
 } else if(3.33 < randomNumber && randomNumber <= 6.67){
-    return "Scissors";
+    return "scissors";
 }else{
-    return "Paper"
+    return "paper"
 }
 }
 
+function getHumanChoice(){
+    let randomChoice = prompt("Rock, Scissors, Paper?", "");
+    randomChoice = randomChoice.toLowerCase();
+    return randomChoice;
+}
 
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+function playRound(humanChoice, computerChoice){
+
+    if(humanChoice == "rock" && computerChoice == "rock"){
+        alert("It is a draw.");
+    }else if (humanChoice == "rock" && computerChoice == "scissors"){
+        alert("You win! Rock beats Scissors.")
+    }else if (humanChoice == "rock" && computerChoice == "paper"){
+        alert("You lose! Paper beats Rock.")
+    }else if (humanChoice == "scissors" && computerChoice == "rock"){
+        alert("You lose! Rock beats Scissors.")
+    }else if(humanChoice == "scissors" && computerChoice == "scissors"){
+        alert("It is a draw.")
+    }else if(humanChoice == "scissors" && computerChoice == "paper"){
+        alert("You win! Scissors beats Paper.")
+    }else if(humanChoice == "paper" && computerChoice == "paper"){
+        alert("It is a draw.")
+    }else if(humanChoice == "paper" && computerChoice == "scissors"){
+        alert("You lose! Scissors beats Paper.")
+    }else if(humanChoice == "paper" && computerChoice == "rock"){
+        alert("You win! Paper beats Rock.")
+    }else{
+        alert("There is an error")
+    }
+       
+    
+}
+
+
+playRound(humanSelection, computerSelection);
 
