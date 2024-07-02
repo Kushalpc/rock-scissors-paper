ce2f3d630b8ddebd35e8b23f1e81c8e4f7989b44
Kushal Parajuli <155858513+Kushalpc@users.noreply.github.com>
2024-07-02 09:53:55 +0545

    add random computer choice

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 'use strict';
-console.log('Hello world!')
+// computer choice
+function getComputerChoice(){
+  let randomNumber = Math.round(Math.random() * 1000) / 100;
+
+  if(randomNumber <= 3.33){
+    return "Rock";
+} else if(3.33 < randomNumber && randomNumber <= 6.67){
+    return "Scissors";
+}else{
+    return "Paper"
+}
+}
+
+
+
